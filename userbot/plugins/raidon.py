e7fdafd5b0f9e02985674ec8bf3986451e8fea45
itsrxns <51463109+itsrxns@users.noreply.github.com>
2020-11-03 01:12:42 +0100

    Create raidon.py

---
+++ userbot/plugins/raidon.py
@@ -0,0 +1,100 @@
+# Copyright © 2020 di 100101110 Github, <https://github.com/100101110>.
+#
+# Questo file fa parte del progetto <https://github.com/100101110/userbot-100101110>,
+# e viene rilasciato in base alla "Licenza GNU Affero General Public v3.0".
+# Si prega di consultare <https://github.com/100101110/userbot-100101110/blob/master/LICENSE>
+#
+# Tutti i diritti riservati.
+# 
+# Crediti: @100101110
+#
+"""Commands:
+.checkcloni"""
+
+import asyncio
+from telethon import events
+from userbot import bot
+from userbot.system import dev_cmd
+
+
+@bot.on(dev_cmd(pattern=f"checkcloni", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return 
+    await event.edit("**🚨 RAID AVVIATO 🚨**")
+    await asyncio.sleep(0.3)
+    await event.edit("**   RAID AVVIATO   **")
+    await asyncio.sleep(0.3)
+    await event.edit("**🚨 RAID AVVIATO 🚨**")
+    await asyncio.sleep(0.3)
+    await event.edit("**🚨 RAID TRA 20 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 19 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 18 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 17 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 16 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 15 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 14 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 13 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 12 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 11 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 10 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 9 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 8 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 7 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 6 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 5 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 4 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 3 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 2 SECONDI 🚨**")
+    await asyncio.sleep(1)
+    await event.edit("**🚨 RAID TRA 1 SECONDO 🚨**")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
